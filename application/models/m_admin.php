97e86e453bbe60d9cd3ef958ec3e623d403a3524
fahmihasan97 <80931334+fahmihasan97@users.noreply.github.com>
2021-03-30 09:17:45 +0800

    baru
    baru

--- application/models/m_admin.php
+++ application/models/m_admin.php
@@ -43,37 +43,7 @@ public function krisis_data()
 
 
 
-public function tpp_id($id='')
-{
-  return $this->db->query("SELECT * from krisis a,overview b ,tpp c , absen d where c.id_krisis='$id' AND a.id_overview=b.id_overview 
-    AND d.id_krisis=a.id_krisis
-    AND c.id_krisis=a.id_krisis
-    group by c.id_tpp");
-}
 
-public function tpp()
-{
-  return $this->db->query("SELECT * from krisis a,overview b ,tpp c , absen d where a.id_overview=b.id_overview 
-    AND d.id_krisis=a.id_krisis
-  	AND c.id_krisis=a.id_krisis
-  	group by c.id_tpp");
-}
-
-
-public function tpp_print($id)
-{
-  return $this->db->query("SELECT * from krisis a,overview b ,tpp c , absen d where a.id_overview=b.id_overview 
-    AND d.id_krisis=a.id_krisis
-  	AND c.id_krisis=a.id_krisis
-  	AND c.id_tpp='$id'
-  	group by c.id_krisis");
-}
-
-
-public function cari_gaji($id='')
-{
-return $this->db->query("SELECT * from krisis a, overview b  where a.id_overview=b.id_overview AND a.id_krisis='$id' group by a.id_krisis");
-}
 
 public function cari_overview($id='')
 {
